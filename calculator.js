f116d3ce53adddc0c10b6a04a43b0e8e5673b53f
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-07-26 10:09:23 +0200

    Create subtract() function

--- calculator.js
+++ calculator.js
@@ -2,3 +2,8 @@ function add(...numbers) {
     let result = numbers.reduce((sum, current) => sum + current, 0);
     return result;
 }
+
+function subtract(...numbers) {
+    let result = numbers.reduce((sum, current) => sum - current);
+    return result;
+}
