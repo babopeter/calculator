75a852b132e5252a1c8741d7f09354ab30ddbb13
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-07-26 10:51:35 +0200

    Create operate() function to switch between operations

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = "+";
+
 function add(...numbers) {
     let result = numbers.reduce((sum, current) => sum + current, 0);
     return result;
@@ -15,5 +19,24 @@ function multiply(...numbers) {
 
 function divide(...numbers) {
     let result = numbers.reduce((acc, current) => acc / current);
+    return result;
+}
+
+function operate(firstNum, operator, secondNum) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(firstNum, secondNum);
+            break;
+        case "-":
+            result = subtract(firstNum, secondNum);
+            break;
+        case "x":
+            result = multiply(firstNum, secondNum);
+            break;
+        case "/":
+            result = divide(firstNum, secondNum);
+            break;
+    }
     return result;
 }
